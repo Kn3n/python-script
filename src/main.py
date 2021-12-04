9ec7f92b8b195ef502e35e50189bb9bcd29ca85c
Joseluu315 <77939750+Joseluu315@users.noreply.github.com>
2021-12-04 21:59:40 +0100

    Write and get the input in .txt file

---
+++ src/main.py
@@ -0,0 +1,21 @@
+'''
+Save the input in to file
+'''
+def main():
+    '''
+    Main function
+    '''
+    # Get the input
+    input_string = input()
+    # Write to file
+    with open('input.txt', 'w') as file:
+        file.write(input_string)
+
+
+
+
+'''
+execute the main function
+'''
+if __name__ == "__main__":
+    main()
