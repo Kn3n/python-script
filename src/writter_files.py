03c1ee1ea75922de0c47673ea1735bd2bb09c723
Joseluu315 <77939750+Joseluu315@users.noreply.github.com>
2021-12-05 21:46:27 +0100

    Update writter_files.py

--- src/writter_files.py
+++ src/writter_files.py
@@ -7,9 +7,11 @@ def main():
     '''
     # Get the input
     input_string = input()
+    print("Write the input to save in .txt archive.")
     # Write to file
     with open('input.txt', 'w') as file:
         file.write(input_string)
+        print('You file has been created with the input')
 
 '''
 Execute the main function
