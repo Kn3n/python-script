b2206ea232f6ae260d08ef61178b435e9cd11f24
Joseluu315 <77939750+Joseluu315@users.noreply.github.com>
2021-12-05 22:06:23 +0100

    Create window.py

---
+++ src/window.py
@@ -0,0 +1,14 @@
+def main():
+    # Importing the necessary modules
+    import tkinter as tk
+    root = tk.Tk()
+    # Setting the title and size of the window
+    root.title('Tic Tac Toe')
+    root.geometry('300x300')
+    root.mainloop()
+    
+'''
+Execute the main function
+'''
+__name__ == '__main__' 
+main()
